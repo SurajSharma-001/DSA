e6f6022a1eb50c0b55ecb6870bfa0a3814b6081d
SurajSharma-001 <surajsharma22491@gmail.com>
2026-07-08 11:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (67.42%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
+        int row = matrix[0].length;
+        int colums = matrix.length;
 
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[][] arr = new int[m][n];
+        int[][] result = new int[row][colums];
 
-        for(int i = 0; i<arr[0].length; i++){
-            for(int j=0; j<arr.length; j++){
-                arr[j][i] = matrix[i][j];
+        for(int i = 0; i < colums;i++){
+            for(int j = 0; j < row; j++){
+                result[j][i] = matrix[i][j];
             }
         }
-        
-        return arr;
+        return result;
     }
- }
+}
+
 // class Solution {
 //     public int[][] transpose(int[][] matrix) {
-//         int row = matrix[0].length;
-//         int colums = matrix.length;
 
-//         int[][] result = new int[row][colums];
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+//         int[][] arr = new int[m][n];
 
-//         for(int i = 0; i < colums;i++){
-//             for(int j = 0; j < row; j++){
-//                 result[j][i] = matrix[i][j];
+//         for(int i = 0; i<arr[0].length; i++){
+//             for(int j=0; j<arr.length; j++){
+//                 arr[j][i] = matrix[i][j];
 //             }
 //         }
-//         return result;
+        
+//         return arr;
 //     }
-// }
+//  }
